675d48b4326f7f2b61003a4b418acc97b1b55014
Cristian Pérez <cristian0923dpl@gmail.com>
2024-06-12 05:09:36 -0500

    URLrender

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api/"
+    // baseURL: "http://localhost:3001/api/"
+    baseURL: "https://integrador-parte3-node-3.onrender.com"
 });
 
 export const getProducts = async () => {
